3e8816570fceb915db7c560ce0ff0fab67937d1e
Arturo Gómez <artuedu@gmail.com>
2020-07-30 18:17:53 -0500

    docs agregado

---
+++ docs/precache-manifest.a742ff7f3f703f9c0a1a476ae857e07a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cf7f9fc8c0f6bea89805d4fed1509d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d637732cdfde3a6ad8b9",
+    "url": "/static/css/main.b1e0e85c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d637732cdfde3a6ad8b9",
+    "url": "/static/js/main.064d1a9a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
